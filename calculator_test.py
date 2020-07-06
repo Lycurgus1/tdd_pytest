d2ced920c90ad60d87f949f1807c2dd5dd07c67c
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-06 12:24:16 +0100

    First commit

---
+++ calculator_test.py
@@ -0,0 +1,20 @@
+import pytest
+import unittest
+
+from calculator import *
+
+class Calc_Test(unittest.TestCase):
+
+    simple_calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.simple_calc.add(2, 2), 4)
+
+    def test_subtract(self):
+        self.assertEqual(self.simple_calc.subtract(3, 1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.simple_calc.multiply(2, 2), 4)
+    #
+    def test_divide(self):
+        self.assertEqual(self.simple_calc.divide(9, 3), 3)
