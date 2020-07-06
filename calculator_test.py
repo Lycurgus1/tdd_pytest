f8ebab1c215e43fb0a409e2457fbcbfe28f15708
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-06 12:33:07 +0100

    Exercise done

--- calculator_test.py
+++ calculator_test.py
@@ -7,14 +7,16 @@ class Calc_Test(unittest.TestCase):
 
     simple_calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(self.simple_calc.add(2, 2), 4)
+    def test_remainder(self):
+        self.assertEqual(self.simple_calc.remainder(2, 2), 0)
 
-    def test_subtract(self):
-        self.assertEqual(self.simple_calc.subtract(3, 1), 2)
+    def test_inches_to_cm(self):
+        self.assertEqual(self.simple_calc.inches_to_cm(1), 2.54)
 
     def test_multiply(self):
         self.assertEqual(self.simple_calc.multiply(2, 2), 4)
     #
     def test_divide(self):
         self.assertEqual(self.simple_calc.divide(9, 3), 3)
+
+# Type python -m pytest to work
