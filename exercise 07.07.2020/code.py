7d82c32f8251353aa5f2472dc1adfa6352ad9f75
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-07 20:25:56 +0100

    exercise

---
+++ exercise 07.07.2020/code.py
@@ -0,0 +1,11 @@
+import math
+
+class Code:
+
+    def find_sqrt(self, num):
+        return math.sqrt(num)
+
+    def find_ceil(self, num):
+        return math.ceil(num)
+
+# HINT:- You can hard code the values in your tests using assert
